dda768caa9bef7e1076e40dd99856800740413f6
nshkrdotcom <127063941+nshkrdotcom@users.noreply.github.com>
2026-05-05 08:32:28 -1000

    Remediate Llama CPP SDK atom cleanup

--- test/llama_cpp_sdk/source_policy_test.exs
+++ test/llama_cpp_sdk/source_policy_test.exs
@@ -12,7 +12,7 @@ defmodule LlamaCppSdk.SourcePolicyTest do
   ]
   @excluded_segments ["deps", "_build", "dist", "doc", "tmp", "node_modules"]
 
-  test "source does not use dynamic atom conversion or regex APIs" do
+  test "source does not use dynamic atom conversion or pattern-engine APIs" do
     violations =
       @source_roots
       |> Enum.flat_map(&Path.wildcard(Path.join(@repo_root, &1)))
@@ -47,7 +47,8 @@ defmodule LlamaCppSdk.SourcePolicyTest do
       {join(["Reg", "Exp"]), join(["Reg", "Exp"])},
       {join(["reg", "exp"]), join(["reg", "exp"])},
       {join(["re", ".compile"]), join(["re", ".compile"])},
-      {join(["import ", "re"]), join(["import ", "re"])}
+      {join(["import ", "re"]), join(["import ", "re"])},
+      {join(["Module", ".concat"]), join(["Module", ".concat"])}
     ]
   end
 
